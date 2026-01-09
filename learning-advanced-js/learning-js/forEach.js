1eb2a303b5abbd526389899ee209040fff2b58d2
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-01-09 12:26:35 +0530

    push arr

--- learning-advanced-js/learning-js/forEach.js
+++ learning-advanced-js/learning-js/forEach.js
@@ -1,9 +1,14 @@
 let arr=[1,2,3,4,5];
 arr[10]=11;
-console.log(Object.keys(arr));
-let str=arr.toString();
-console.log(str);
-console.log(typeof str);
-console.log(str.padStart(1,'011'));
-
-
+//console.log(Object.keys(arr));
+// let str=arr.toString();
+// console.log(str);
+// console.log(typeof str);
+// console.log(str.padStart(1,'011'));
+ arr.forEach(function(value,index){
+    console.log(`index:${index}` ,`value:${value}`);
+});
+console.log(arr.reverse());
+// console.log(arr);console.log(arr.length);
+let reversedArr = arr.toReversed();
+console.log(reversedArr);
