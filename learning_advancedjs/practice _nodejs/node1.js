18e53941c73568c8f22fa2b8aec6b38342ae10aa
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-01-17 00:59:50 +0530

    server clear

--- learning_advancedjs/practice _nodejs/node1.js
+++ learning_advancedjs/practice _nodejs/node1.js
@@ -1,7 +1,15 @@
 const http=require('http');
 const server=http.createServer((req,res)=>
     {
-        
-    res.end("hello server");
-});
+        if (req.url==='/'){
+            res.end("home page");
+        }
+        else if (req.url==='/about'){
+            res.end("about page");
+            console.log("about page visited");
+        }
+        else{
+            res.end("error page");
+        }
+    });
 server.listen(4000);
