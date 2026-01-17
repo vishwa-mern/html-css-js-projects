ac6109085610227c6c59fe4455da88568829c32b
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-01-17 14:59:16 +0530

    push

--- learning_advancedjs/practice _nodejs/node1.js
+++ learning_advancedjs/practice _nodejs/node1.js
@@ -1,16 +1,25 @@
-const http=require('http');
-const server=http.createServer((req,res)=>
-    {
-        if (req.url==='/' && req.method==='GET'){
-            res.end("home page");
-            
-        }
-        else if (req.url==='/about' && req.method==='POST'){
-            res.end("about page");
-            console.log("about page visited");
-        }
-        else{
-            res.end("error page");
-        }
-    });
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === "/" && req.method === "GET") {
+
+    // ⭐ IMPORTANT LINE
+    res.setHeader("Content-Type", "text/html");
+
+    res.end(`
+      <h2>Home Page</h2>
+      <form action="/about" method="POST">
+        <button type="submit">POST bhejo</button>
+      </form>
+    `);
+  } 
+  else if (req.url === "/about" && req.method === "POST") {
+    res.end("about page");
+    console.log("about page visited");
+  } 
+  else {
+    res.end("404 page"); 
+  }
+})
 server.listen(4000);
