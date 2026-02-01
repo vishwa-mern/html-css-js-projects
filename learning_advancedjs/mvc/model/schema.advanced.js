de016fe708caaae3a29ff1f0ccd00cd63a697c95
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-02-02 00:32:34 +0530

    push

--- learning_advancedjs/mvc/model/schema.advanced.js
+++ learning_advancedjs/mvc/model/schema.advanced.js
@@ -1,1 +1,12 @@
 const mongoose= require("mongoose");
+
+const schemaName = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    }
+    time:{
+        type:String
+    }
+});
+module.exports=mongoose.model("Attendence",schemaName);
