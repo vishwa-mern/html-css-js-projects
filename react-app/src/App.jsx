55ffbed87829f525563e2c20b4421c9a54d145b2
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-02-06 01:09:24 +0530

    update react

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-//import './App.css'
 
 function App() {
-  
-
+ const name="Vishwajeet"
   return (
     <div>
       <h1>
-        Hallo Vishwajeet 
+        Hallo my name is {name}
+        <br />
+       
+      </h1>
+       <Learn/>
+    </div>
+  )
+}
+
+function Learn(){
+  return(
+    <div>
+      <h1>
+         <ul>
+          <li>
+              i am learning react js.
+          </li>
+         </ul>
       </h1>
     </div>
   )
