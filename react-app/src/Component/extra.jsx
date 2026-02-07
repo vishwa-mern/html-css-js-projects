0c2d57f456926ae1d5a6ef810dab1b7c48040dcb
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-02-07 05:45:39 +0530

    learn props

---
+++ react-app/src/Component/extra.jsx
@@ -0,0 +1,16 @@
+function Extra(props){
+    return(
+
+        <div>
+            <h1>
+                {props.title} will be held in {props.year}
+                <ul>
+                    <li>India</li>
+                    <li>Australia</li>
+                    <li>England</li>    
+                </ul>
+            </h1>
+        </div>
+    )
+}
+export default Extra
