e2cc476d0ba67252b5131e0cf8d2ca70baee86b5
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-02-12 21:19:50 +0530

    use transition and transform

--- react-app/src/Component/navbar1.jsx
+++ react-app/src/Component/navbar1.jsx
@@ -9,12 +9,13 @@ function Navbar1(){
         </div>
         <div className="nav-item">
             <h5>
-                <Link to="/" style={{textDecoration:"none"}}>Home</Link>
-                <Link to="/about" style={{textDecoration:"none"}}>About</Link>
-                <Link to="/portfolio" style={{textDecoration:"none"}}>Portfolio</Link>
-                <Link to="/services" style={{textDecoration:"none"}}>Services</Link>
-                <Link to="/contact" style={{textDecoration:"none"}}>Contact</Link>
+                <Link to="/" style={{textDecoration:"none"}} className="nav-link">Home</Link>
+                <Link to="/about" style={{textDecoration:"none"}} className="nav-link">About</Link>
+                <Link to="/portfolio" style={{textDecoration:"none"}} className="nav-link">Portfolio</Link>
+                <Link to="/services" style={{textDecoration:"none"}} className="nav-link">Services</Link>
+                <Link to="/contact" style={{textDecoration:"none"}} className="nav-link">Contact</Link>
             </h5>
+            <button className="btn">Login</button>
         </div>
         </div>
         </>
