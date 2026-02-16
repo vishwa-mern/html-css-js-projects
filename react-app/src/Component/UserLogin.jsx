afd20b61f5970915532eed78ed1d7c96a0f3c493
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-02-16 18:57:55 +0530

    create form page data

--- react-app/src/Component/UserLogin.jsx
+++ react-app/src/Component/UserLogin.jsx
@@ -1,18 +1,22 @@
+import "./userlogin.css"
+import { useState } from "react";
 const Login=()=>{
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    console.log(username,email,password);
     return (
         <div>
             <h2>Login Component</h2>
             <form className="login-form">
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" required />
-                <br />
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
-                <br />
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required />
-                <br />
-                <button type="submit">Login</button>
+                
+                <input type="text" id="username" name="username" placeholder="enter your username" onChange={(e)=>setUsername(e.target.value)}/> 
+                 
+                <input type="email" id="email" name="email" placeholder="enter your email" onChange={(e)=>setEmail(e.target.value)}/> 
+                
+                <input type="password" id="password" name="password" placeholder="enter your password" onChange={(e)=>setPassword(e.target.value)}/> 
+            
+                <input type="submit" id="button" value="Login" />    
 
             </form>
         </div>
