61ea7e4fb29824915c018d1df8f78d3419d0e199
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-04-03 22:30:36 +0530

    push

--- learning_advancedjs/node_revision.js/node1.js
+++ learning_advancedjs/node_revision.js/node1.js
@@ -1,6 +1,11 @@
-let http = require('http');
-http.createServer((req,res)=>{
-    res.writeHead(200, {'Content-Type': 'text/html'});
-res.write("<h1>Hello my name is vishwajeet <br/> i am a node.js developer</h1>");
-res.end();
-}).listen(3000);
+const http = require('http');
+
+http.createServer((req, res) => {
+    
+  console.log("Request aayi:", req.url);
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hello, World!</h1> <p style="color: blue;">Welcome to my Node.js server.</p>  ');
+}).listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
