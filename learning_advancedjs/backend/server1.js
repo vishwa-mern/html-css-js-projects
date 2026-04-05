72c77679599da994b18ec3c9897b3d6c79e3104f
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-04-06 01:24:35 +0530

    connect with sql

--- learning_advancedjs/backend/server1.js
+++ learning_advancedjs/backend/server1.js
@@ -4,5 +4,24 @@ const cors=require("cors");
 const app=express();
 app.use(express.json());
 app.use(cors());
+const db=sql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"vedant8592",
+    database:"vishwa123"    
+})
 
-app
+
+app.get("/",(req,res)=>{
+    res.end("from backend server 1 😀 ");
+})
+app.get("/data",(req,res)=>{
+    let sql=`SELECT * FROM employee_details`;
+    db.query(sql,(err,result)=>{
+        if(err) return res.status(500).json({error: "Database query failed"});
+        res.json(result);
+    });
+});
+app.listen(4000,()=>{
+    console.log("server is running on port 4000");
+});
