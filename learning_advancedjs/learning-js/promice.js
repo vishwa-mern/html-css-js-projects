8037065575bd39694c0b4a19c5e337f0ce178821
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-09-03 21:06:19 +0530

    push

--- learning_advancedjs/learning-js/promice.js
+++ learning_advancedjs/learning-js/promice.js
@@ -1,18 +1,18 @@
-// const promise_demo = new Promise((resolve,reject)=>{
-//     let a=10;
-//     let b=20;
-//     if(a>b){
-//         resolve("a is greater than b");
-//     }
-//     else{
-//         reject("a is not greater than b");
-//     }
-// });
-// promise_demo.then((message)=>{
-//     console.log("Promise resolved:", message);
-// }).catch((error)=>{
-//     console.log("Promise rejected:", error);
-//});
+const promise_demo = new Promise((resolve,reject)=>{
+    let a=10;
+    let b=20;
+    if(a>b){
+        resolve("a is greater than b");
+    }
+    else{
+        reject("a is not greater than b");
+    }
+});
+promise_demo.then((message)=>{
+    console.log("Promise resolved:", message);
+}).catch((error)=>{
+    console.log("Promise rejected:", error);
+});
 
 // async function test() {
 //   return "Hello";
@@ -21,14 +21,14 @@
 //   console.log("Promise resolved:", message);
 // }).catch((error) => {
 //   console.log("Promise rejected:", error);
-//}); 
+// }); 
 
-function getData(){
-  return Promise.resolve("Data mil gaya");
-}
-async function showData() {
-  const result = await getData();
-  console.log(result);
-}
-showData();
+// function getData(){
+//   return Promise.resolve("Data mil gaya");
+// }
+// async function showData() {
+//   const result = await getData();
+//   console.log(result);
+// }
+// showData();
 
