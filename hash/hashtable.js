869212575ed0311acaf7c233a275f5593a8c2be2
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-09-09 11:06:31 +0530

    searching element in has

--- hash/hashtable.js
+++ hash/hashtable.js
@@ -1,14 +1,10 @@
 
 
-function hash(key){
+function hash(key, callback){
     
 let hash= new Array(10);
 let sum = 0
 let strs=0
-
-
-
-
 
 for (let i=0; i<key.length; i++){
     for (let j=0; j<key[i].length; j++){
@@ -22,11 +18,34 @@ for (let i=0; i<key.length; i++){
     
 
 }
-return hash
+
+callback(hash);
+
+
+
+return hash;
+}
+
+function contains(value)
+{
+    let sum=0
+    for (let i=0; i<value.length; i++){
+        let char = value.charCodeAt(i);
+        sum += char;
+    }
+    sum= sum % 10;
+    return function(hash){
+        if (hash[sum] === value){
+     console.log(true);
+    } else {
+        console.log(false);
+    }
+  
+}
 }
 
 my_array = ['Pete', 'Jones', 'Lisa', 'Bob', 'Siri']
-console.log(hash(my_array))
+console.log(hash(my_array,contains('Pets')))
 
 
 
