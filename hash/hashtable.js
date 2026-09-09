0d8a1aa419435ec85f39640a9cfb24f037ddf851
Vishwajeet Birajdar <birajdarvishwajeet931@gmail.com>
2026-09-09 13:06:20 +0530

    open hashing is done

--- hash/hashtable.js
+++ hash/hashtable.js
@@ -1,8 +1,11 @@
 
 
-function hash(key, callback){
+function hash(key){
     
 let hash= new Array(10);
+for (let i = 0; i < hash.length; i++) {
+    hash[i] =[];
+}
 let sum = 0
 let strs=0
 
@@ -13,39 +16,43 @@ for (let i=0; i<key.length; i++){
        
     }
      strs= sum % 10;
-       hash[strs] = key[i];
+       hash[strs].push(key[i]);
         sum = 0
     
 
 }
 
-callback(hash);
+
 
 
 
 return hash;
 }
 
-function contains(value)
-{
-    let sum=0
-    for (let i=0; i<value.length; i++){
-        let char = value.charCodeAt(i);
-        sum += char;
-    }
-    sum= sum % 10;
-    return function(hash){
-        if (hash[sum] === value){
-     console.log(true);
-    } else {
-        console.log(false);
-    }
+// function contains(value)
+// {
+//     let sum=0
+//     for (let i=0; i<value.length; i++){
+//         let char = value.charCodeAt(i);
+//         sum += char;
+//     }
+//     sum= sum % 10;
+//     return function(hash){
+//         if (hash[sum] === value){
+//      console.log("the value is in the hash table",true);
+//     } else {
+//         console.log("the value is not in the hash table:",false);
+//     }
   
-}
-}
+// }
+// }
 
-my_array = ['Pete', 'Jones', 'Lisa', 'Bob', 'Siri']
-console.log(hash(my_array,contains('Pets')))
+
+    
+
+
+my_array = ['Pete', 'Jones', 'Lisa', 'Bob', 'Siri','Stuart']
+console.log(hash(my_array))
 
 
 
